44f950d459b185537d2e1158d327508511d4e394
huichuan <lhc14124908@163.com>
2020-01-27 16:56:46 +0800

    add

---
+++ route/5.3/indexController.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, Blueprint, request
+
+'''
+post/index 列表
+post/info 详情
+post/set 添加|编辑
+post/ops 操作（删除|恢复）
+'''
+
+index_page = Blueprint("index_page", __name__)
+
+
+@index_page.route("/")
+def index_page_index():
+    return "index page"
+
+
+@index_page.route("/me")
+def hello():
+    return "hello ,I Love Imooc"
+
+
+@index_page.route("/get")
+def get():
+    var_a = request.args.get("a", "huichuan");
+    return "request:%s,params:%s,var_a:%s" % (request.method, request.args, var_a)
+
+
+@index_page.route("/post", methods=["POST"])
+def post():
+    return "ok"
