58fe2efd145730c36a3fba2497cf10b57cdf1133
huichuan <lhc14124908@163.com>
2020-01-28 16:38:01 +0800

    add

--- project/config/local_setting.py
+++ project/config/local_setting.py
@@ -7,3 +7,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = "mysql://root:root@127.0.0.1/movie_cat"
 
 SECRET_KEY = "huichuan123456"
+
+DOMAIN = {
+    "www":"http://0.0.0.0:5000"
+}
--- project/www.py
+++ project/www.py
@@ -4,7 +4,6 @@ from controllers.index import index_page
 from flask_debugtoolbar import DebugToolbarExtension
 from controllers.member import member_page
 
-
 # 添加debug tool
 toolbar = DebugToolbarExtension(app)
 
@@ -15,4 +14,12 @@ from interceptors.Auth import *
 from interceptors.errorHandler import *
 
 app.register_blueprint(index_page, url_prefix="/")
-app.register_blueprint( member_page,url_prefix = "/member" )
+app.register_blueprint(member_page, url_prefix="/member")
+
+'''
+模板函数
+'''
+from common.lib.UrlManager import UrlManager
+
+app.add_template_global(UrlManager.buildStaticUrl, 'buildStaticUrl')
+app.add_template_global(UrlManager.buildUrl, 'buildUrl')
